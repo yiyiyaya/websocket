1cee2a0284a614d05c33237b2183c6eb91a69e27
yiyiyaya <15712974451@163.com>
2021-09-08 02:45:19 +0800

    this is a test.

--- impl/connection.go
+++ impl/connection.go
@@ -6,20 +6,20 @@ import (
 	"sync"
 )
 
-type Connection struct{
-	wsConn *websocket.Conn
-	inChan chan[]byte
-	outChan chan[]byte
+type Connection struct {
+	wsConn    *websocket.Conn
+	inChan    chan []byte
+	outChan   chan []byte
 	closeChan chan byte
-	mutex sync.Mutex
-	isClosed bool
-
+	mutex     sync.Mutex
+	isClosed  bool
 }
-func InitConnect(wsConn *websocket.Conn)(conn *Connection, err error){
+
+func InitConnect(wsConn *websocket.Conn) (conn *Connection, err error) {
 	conn = &Connection{
-		wsConn: wsConn,
-		inChan: make(chan []byte, 1000),//如果满了就无法放进去
-		outChan: make(chan []byte, 1000),
+		wsConn:    wsConn,
+		inChan:    make(chan []byte, 1000), //如果满了就无法放进去
+		outChan:   make(chan []byte, 1000),
 		closeChan: make(chan byte, 1),
 	}
 	//启动读协程
@@ -28,27 +28,27 @@ func InitConnect(wsConn *websocket.Conn)(conn *Connection, err error){
 	go conn.WriteLoop()
 	return
 }
-func (conn *Connection)ReadMessage()(data[]byte, err error){
+func (conn *Connection) ReadMessage() (data []byte, err error) {
 	select {
 	case data = <-conn.inChan:
 
-	case <- conn.closeChan:
+	case <-conn.closeChan:
 		err = errors.New("connection is closed")
 	}
 	return data, err
 }
-func (conn *Connection)WriteMessage(data[]byte)(err error){
+func (conn *Connection) WriteMessage(data []byte) (err error) {
 	select {
 	case conn.outChan <- data:
 
-	case <- conn.closeChan:
+	case <-conn.closeChan:
 		err = errors.New("connection is closed")
 	}
 	return err
 }
-func (conn *Connection) Close (){
+func (conn *Connection) Close() {
 	conn.mutex.Lock()
-	if !conn.isClosed{
+	if !conn.isClosed {
 		//保证这一行代码只执行一次
 		close(conn.closeChan)
 		conn.isClosed = true
@@ -58,40 +58,41 @@ func (conn *Connection) Close (){
 	conn.wsConn.Close()
 
 }
+
 //内部实现
-func (conn *Connection) ReadLoop (){
-	var(
+func (conn *Connection) ReadLoop() {
+	var (
 		data []byte
-		err error
+		err  error
 	)
-	for{
-		if _, data, err = conn.wsConn.ReadMessage(); err != nil{
+	for {
+		if _, data, err = conn.wsConn.ReadMessage(); err != nil {
 			goto ERR
 		}
 		select {
 		case conn.inChan <- data:
-		case <- conn.closeChan:
+		case <-conn.closeChan:
 			goto ERR
 		}
 
 	}
-	//阻塞在这里，等待inchan有空闲位置
-	ERR:
-		conn.Close()
+	//阻塞在这里，等待inchan有空闲位置 test
+ERR:
+	conn.Close()
 }
-func (conn *Connection) WriteLoop (){
-	var(
+func (conn *Connection) WriteLoop() {
+	var (
 		data []byte
-		err error
+		err  error
 	)
-	for{
+	for {
 		select {
-		case data = <- conn.outChan:
-		case <- conn.closeChan:
+		case data = <-conn.outChan:
+		case <-conn.closeChan:
 			goto ERR
 		}
 
-		if err = conn.wsConn.WriteMessage(websocket.TextMessage, data); err != nil{
+		if err = conn.wsConn.WriteMessage(websocket.TextMessage, data); err != nil {
 			goto ERR
 		}
 
